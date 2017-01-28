344e63eabdbf5574cd86b7d8ae4c89aedb3aef17
Kiddi <kiddinn95@gmail.com>
2017-01-28 23:47:37 +0000

    some more functionality

--- V1/server/src/aimfire.js
+++ V1/server/src/aimfire.js
@@ -1,16 +1,18 @@
+//setting up canvas
 var canvas = document.getElementById("AmazingCanvas");
 var context = canvas.getContext("2d");
 
 canvas.width = window.innerWidth - 6;
-canvas.height = window.innerHeight - 100;
+canvas.height = window.innerHeight - 150;
 
+//Setting initial values
 var penSize = 5;
 var draw = false;
 var circleRadius = 40;
-
-//penSize is a radius, therefore x2
+var currentFunc = startDrawing;
 context.lineWidth = penSize * 2;
 
+//set radius of circles drawn by drawCircle(e)
 function setCircleRadius() {
 	newRadius = parseInt(document.getElementById("circleradius").value)
 	if(newRadius > 0) {
@@ -18,6 +20,7 @@ function setCircleRadius() {
 	}
 }
 
+//set pen size
 function setPenSize() {
 	newSize = parseInt(document.getElementById("pensize").value)
 	if(newSize > 0) {
@@ -26,6 +29,7 @@ function setPenSize() {
 	}
 }
 
+//the following three handle free-hand drawing
 function startDrawing(e) {
 	context.moveTo(e.clientX, e.clientY);
 	draw = true;
@@ -41,7 +45,6 @@ function mouseDraw(e) {
 	if(draw){
 		context.lineTo(e.clientX, e.clientY);
 		context.stroke();
-		beginPath();
 		context.arc(e.clientX, e.clientY, penSize, 0, 2*Math.PI);
 		context.fill();
 		context.beginPath();
@@ -64,7 +67,6 @@ function setFuncCircle() {
 	currentFunc = drawCircle;
 }
 
-var currentFunc = startDrawing;
 canvas.addEventListener("mousedown", currentFunc);
 var currentUp = stopDrawing;
 canvas.addEventListener("mouseup", currentUp);
