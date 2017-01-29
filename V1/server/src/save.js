1b3000d2c4b0422751d068c9076ed0ed801102ef
jonh <jonhs15@ru.is>
2017-01-29 23:18:44 +0000

    the broken

--- V1/server/src/save.js
+++ V1/server/src/save.js
@@ -3,6 +3,16 @@
 function saveImage(){
   var data = canvas.toDataURL();
 
+  var request = new XMLHttpRequest();
 
-  window.open(data, 'blank','location=0, menubar=0');
+  request.onreadystatechange = function (){
+    if(request.readyState == 4 && request.satus == 200){
+      var resp = request.responseText;
+      window.open(data, 'blank','location=0, menubar=0');
+    }
+  }
+  request.open('POST', 'save.php', true);
+  request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  request.send('img='+data);
+  //window.open(data, 'blank','location=0, menubar=0');
 }
