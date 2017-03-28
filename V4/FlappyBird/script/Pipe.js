e6e04f87ad419dbf4d2fddd4767cacafa627db13
jonh <jonhs15@ru.is>
2017-03-28 16:24:36 +0000

    pipep

---
+++ V4/FlappyBird/script/Pipe.js
@@ -0,0 +1,13 @@
+const Pipe = function(x, y, context){
+    this.x = x;
+    this.y = y;
+    this.context = context;
+}
+
+Pipe.prototype.update = function(){
+
+}
+
+Pipe.prototype.render = function(){
+    
+}
