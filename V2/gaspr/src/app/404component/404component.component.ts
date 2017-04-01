0f7bdb4343fe0c6636466897b58ac00ecd705063
Kiddi <kiddinn95@gmail.com>
2017-04-01 17:16:51 +0000

    login screen

---
+++ V2/gaspr/src/app/404component/404component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-404component',
+  templateUrl: './404component.component.html',
+  styleUrls: ['./404component.component.css']
+})
+export class NotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- V2/gaspr/src/app/app.module.ts
+++ V2/gaspr/src/app/app.module.ts
@@ -3,17 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AlertModule } from 'ng2-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './404component/404component.component';
 import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NotFoundComponent}
+  ]
 
 @NgModule({
   declarations: [
     AppComponent,
+    NotFoundComponent,
     LoginComponent
   ],
   imports: [
     AlertModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
